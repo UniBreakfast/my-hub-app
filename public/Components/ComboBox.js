9385cf511e5ca39c8424fa74c1acf07940f94e56
unibreakfast <unibreakfast1@gmail.com>
2019-11-14 19:55:12 +0200

    cancel, edit, add buttons work

--- public/Components/ComboBox.js
+++ public/Components/ComboBox.js
@@ -12,25 +12,32 @@ export default class ComboBox {
   constructor (domel) {
     this.domel = domel
     this.render()
-    // domel.innerHTML = this.render()
     this.styleUp()
 
-    // add.onclick = cancel.onclick =()=>
-    //   document.querySelectorAll('.combo-select>div')
-    //     .forEach(div => div.classList.toggle('active'))
-
   }
+
+  add =()=> (this.input.value = '', this.toggle())
+
+  edit =()=> (this.input.value = this.select.value, this.toggle())
+
+  toggle =()=>
+    this.domel.qsel('div').forEach(div => div.classList.toggle('active'))
 
   render() {
     const [ [ div1, input ], [ div2, select ] ] = ['input', 'select']
       .map(tag => [credom('div', {className: 'cs-'+tag}), credom(tag)])
-    const [ save, cancel, remove, add ] = ['Save', 'Cancel', 'Remove', 'Add']
-      .map(innerText => credom('button', {innerText}))
+    const [ save, cancel, remove, edit, add ] =
+      ['Save', 'Cancel', 'Remove', 'Edit', 'Add']
+        .map(innerText => credom('button', {innerText}))
     !['Volvo','Saab','Mercedes','Audi'].forEach(value =>
       select.append(credom('option', {value, innerText:value})))
-    ![[div1, input,' ',save,' ',cancel], [div2, select,' ',remove,' ',add],
-      [this.domel,div1,div2]].forEach(([domel,...kids])=> domel.append(...kids))
+    ![[div1, input, ' ', save, ' ', cancel],
+      [div2, select,' ', remove,' ', edit,' ', add], [this.domel, div1, div2]]
+        .forEach(([domel,...kids])=> domel.append(...kids))
+    ![[cancel,'toggle'],[add,'add'],[edit,'edit']]
+      .map(([btn, method]) => btn.onclick = this[method])
     div1.classList.add('active')
+    assign(this, {input, select})
   }
 
   styleUp() {
@@ -38,12 +45,13 @@ export default class ComboBox {
     if (d.qsel('#'+id, 1)) return
     d.head.append(credom('style', {id, innerHTML: css`
       .combo-select input, .combo-select select {
-        width: 200px;
+        width: 12rem;
         height: 2rem;
         padding: .3rem;
         box-sizing: border-box;
       }
-      .combo-select>div:not(.active) { display: none } `
+      .combo-select>div:not(.active) { display: none }
+      .cs-input button { padding-left: 1.08rem; padding-right: 1.08rem } `
     }))
   }
 }
--- public/spaloader.js
+++ public/spaloader.js
@@ -1,10 +1,12 @@
 const html = raw => raw[0]
+const { assign } = Object
 
 import ComboBox from './Components/ComboBox.js'
 
 document.body.innerHTML += html`<h2>Added via JS</h2>`
 
-document.querySelectorAll('.combo-select').forEach(div => new ComboBox(div))
+const [ cb1, cb2 ] =
+  [...document.querySelectorAll('.combo-select')].map(div => new ComboBox(div))
 
 Object.defineProperty(Object.prototype, 'c', {
   get() {
@@ -13,3 +15,5 @@ Object.defineProperty(Object.prototype, 'c', {
     return this
   }
 })
+
+assign(window, {cb1, cb2})
